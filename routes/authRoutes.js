ee5c12fcf0830cf25afb90aba37a92077455be07
basantpatidar <basantpatidar33@gmail.com>
2019-08-04 04:27:16 -0400

    Added billing and client side app

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,11 +10,17 @@ module.exports = app => {
   );
 
   // Got the Code and fetching user Profile
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
   app.get("/api/logout", (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect("/");
   });
 
   app.get("/api/current_user", (req, res) => {
--- services/passport.js
+++ services/passport.js
@@ -23,22 +23,16 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      // console.log(accessToken);
-      // console.log("refresh token", refreshToken);
-      // console.log("profile", profile);
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
 
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          //we already have  record with the given profile id
-          done(null, existingUser);
-        } else {
-          //We dont have a user record with the given profile id and we have to create new one.
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+      if (existingUser) {
+        //we already have  record with the given profile id
+        return done(null, existingUser);
+      }
+      //We dont have a user record with the given profile id and we have to create new one.
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
