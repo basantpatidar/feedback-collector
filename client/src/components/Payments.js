c50ca232ef4c2482bed97d2ecbe50c02563ace64
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-14 01:52:47 -0400

    payment button added l102

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,23 +1,26 @@
-// import React, { Component } from "react";
-// import StripeCheckout from "react-stripe-checkout";
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
 // import { connect } from "react-redux";
 // import * as actions from "../actions";
 
-// class Payments extends Component {
-//   render() {
-//     return (
-//       <StripeCheckout
-//         name="Emaily"
-//         description="$5 for % email credits"
-//         amount={500}
-//         token={token => this.props.handleToken(token)}
-//         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-//       >
-//         <button className="btn">Add credits</button>
-//       </StripeCheckout>
-//     );
-//   }
-// }
+class Payments extends Component {
+  render() {
+    // debugger;
+    return (
+      <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
+        amount={500}
+        // token={token => this.props.handleToken(token)}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn red darken-4">Add credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+export default Payments;
 
 // export default connect(
 //   null,
