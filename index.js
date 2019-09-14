73457a7f4eb54dce37a6322bda122f15967f607c
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-14 13:47:00 -0400

    ready for production

--- index.js
+++ index.js
@@ -31,23 +31,23 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+//both these require statements return their respective app functions
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
-//both these require statements return their respective app functions
-// require("./routes/billingRoutes")(app);
 
+//getting app ready for production environment
 if (process.env.NODE_ENV === "production") {
   //Express will serve up production assets
   // like our main.js file, or main.css file
   //
-  // app.use(express.static("client/build"));
-  // //Express will serve up the index.html file
-  // // if it doesnt recognize the route
-  //
-  // const path = require("path");
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+  app.use(express.static("client/build"));
+  // Express will serve up the index.html file
+  //  if it doesnt recognize the route
+  // All other request are sent to this route "client/build/index.html"
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
