7a740c5bc8e865b0ca72a1e136dce630d12b3a6f
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-14 02:33:46 -0400

    body parder installed and wired on index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -18,7 +18,9 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-// app.use(bodyParser.json());
+//All the middleware needs to be wired with "app.use"
+
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -30,6 +32,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 //both these require statements return their respective app functions
 // require("./routes/billingRoutes")(app);
 
