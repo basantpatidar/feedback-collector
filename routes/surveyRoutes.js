99e468ea64ae2662824bbf968c71b9341bcf9a11
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-15 22:54:16 -0700

    survey  instance created

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,25 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+
+//getting access to survey model
+const Survey = mongoose.model("surveys");
+
 module.exports = app => {
-  app.post("api/surveys", requireLogin, (req, res) => {});
+  app.post("api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const servey = new servey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => {
+        return {
+          email: email
+        };
+      }),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+  });
 };
