cd109295cb366e88cc8c60f336c11553af4e43cf
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-20 02:26:03 -0400

    /api/surveys error corrected

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,18 +8,14 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.post("api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title,
       subject,
       body,
-      recipients: recipients.split(",").map(email => {
-        return {
-          email: email.trim()
-        };
-      }),
+      recipients: recipients.split(",").map(email => ({ email })),
       _user: req.user.id,
       dateSent: Date.now()
     });
