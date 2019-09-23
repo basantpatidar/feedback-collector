2fb67a196497afa8b20926f63a2758596666c995
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-23 19:46:07 -0400

    l153 surveyForm Field and Button

--- client/src/components/surveys/surveyForm.js
+++ client/src/components/surveys/surveyForm.js
@@ -1,10 +1,17 @@
 //surveyForm Shows Survey form for a user to add input
 import React from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends React.Component {
   render() {
-    return <div>SurveyForm</div>;
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field type="text" name="surveyTitle" component="input" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 export default reduxForm({
