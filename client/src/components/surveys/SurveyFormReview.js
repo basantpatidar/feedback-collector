97046bd609f1c4eac3ab92789e58656a48a3d057
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-25 21:35:44 -0400

    SurveyFormReview boiler plate

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SurveyReview = () => {
+  return (
+    <div>
+      <h5>Please check once before confirming</h5>
+    </div>
+  );
+};
+export default SurveyReview;
