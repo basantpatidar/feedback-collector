de92c71b15ef7e7490b36196c224b6b473f76e04
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-26 20:36:02 -0400

    surveyForm clears on back

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,5 +1,6 @@
 //SurveyNew is parent component to SurveyForm and SurveyFormReview
 import React from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
@@ -25,4 +26,6 @@ class SurveyNew extends React.Component {
     return <div>{this.renderContent()}</div>;
   }
 }
-export default SurveyNew;
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew);
