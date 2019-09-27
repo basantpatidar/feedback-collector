ae10250a787fd2d302ae6e71e60818245217a4d8
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2019-09-26 20:35:32 -0400

    l171 action  creator submitSurvey created n wired

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,11 @@
 //Survey Confirmation page
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
-import _ from 'lodash';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={name}>
@@ -24,7 +25,10 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
       >
         Back
       </button>
-      <button className="green btn-flat right white-text">
+      <button
+        className="green btn-flat right white-text"
+        onClick={() => submitSurvey(formValues)}
+      >
         Send Survey
         <i className="material-icons right">email</i>
       </button>
@@ -36,4 +40,7 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(
+  mapStateToProps,
+  actions
+)(SurveyFormReview);
